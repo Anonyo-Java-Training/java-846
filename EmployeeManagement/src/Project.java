6d673279df31197b58dda475f1efb4113a5c1e1c
Anonyo <Anonyo@LAPTOP-LPCVOOVC>
2020-10-15 15:30:48 +0530

    Modify project

--- EmployeeManagement/src/Project.java
+++ EmployeeManagement/src/Project.java
@@ -1,5 +1,26 @@
 
 public class Project {
+	int projectCode;
 	private String projectName;
+	int projectDuration;
+	public int getProjectCode() {
+		return projectCode;
+	}
+	public void setProjectCode(int projectCode) {
+		this.projectCode = projectCode;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public int getProjectDuration() {
+		return projectDuration;
+	}
+	public void setProjectDuration(int projectDuration) {
+		this.projectDuration = projectDuration;
+	}
+	
 
 }
